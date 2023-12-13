c15e872bc3069e0095492c4dc9bd6a02d44a4e10
konbraphat51 <konbraphat@gmail.com>
2023-12-14 04:43:01 +0900

    Add: _computeminkowski edge

--- Public/Collision.js
+++ Public/Collision.js
@@ -6,15 +6,20 @@
  * @param {Polygon} polygon1 polygon
  * @returns {boolean} true if the polygons are colliding, false otherwise
  */
-function Is2PolygonsColliding(polygon0, polygon1) {}
+function Is2PolygonsColliding(polygon0, polygon1) {
+	//initial vectors
+	let vector0 = [1, 0]
+	let vector1 = [-1, 0]
+	let vector2 = [0, 1]
+}
 
 /**
  * Support function for GJK algorithm.
  * @param {Polygon} polygon
  * @param {number[]} direction
- * @returns {number[][]} [[most plus position], dot product]
+ * @returns {number[]} most plus position
  */
-function _GetSupportPoint(polygon, direction) {
+function _ComputeSupportPoint(polygon, direction) {
 	let maxDot = -Infinity
 	let maxPos = null
 
@@ -30,5 +35,19 @@ function _GetSupportPoint(polygon, direction) {
 		}
 	}
 
-	return [maxPos, maxDot]
+	return maxPos
+}
+
+/**
+ * Compute the Edge for the directoin vector  of Minkowski difference of two polygons.
+ * @param {Polygon} polygon0
+ * @param {Polygon} polygon1
+ * @param {number[]} direction vector
+ * @returns {number[]} edge vector
+ */
+function _ComputeMinkowskiEdge(polygon0, polygon1, direction) {
+	let support0 = _ComputeSupportPoint(polygon0, direction)
+	let support1 = _ComputeSupportPoint(polygon1, MultiplyVec(-1, direction))
+
+	return SubVec(support0[0], support1[0])
 }
